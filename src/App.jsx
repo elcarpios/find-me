8305a51397b8c907869f0d69dc8ced9d6fc1c4ef
Luis Alberto Fernandez Calleja <alberto.fernandez@marfeel.com>
2021-07-20 07:49:15 +0200

    instaled styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled, { css } from 'styled-components';
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+
+  ${(props) =>
+    props.primary &&
+    css`
+      background: palevioletred;
+      color: white;
+    `}
+`;
+const Container = styled.div`
+  text-align: center;
+`;
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,35 +29,20 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello! This is Alberto + Carpio!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            How much we rule: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
       </header>
+      <Container>
+        <Button type="button" onClick={() => setCount((count) => count + 1)}>
+          How much we rule: {count}
+        </Button>
+        <Button
+          primary
+          onClick={() => {
+            window.location = 'https://github.com/elcarpios/find-me';
+          }}
+        >
+          Primary Button!
+        </Button>
+      </Container>
     </div>
   );
 }
