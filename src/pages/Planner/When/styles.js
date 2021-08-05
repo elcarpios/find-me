52bab4b7688807f117872618558e1e75f1c57b29
elcarpios <elcarpiosbit@gmail.com>
2021-08-06 00:21:58 +0200

    Change strategy for sizing height in where list again

--- src/pages/Planner/When/styles.js
+++ src/pages/Planner/When/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { Title, Container, Pill, Main } from 'styles/components';
 
 const List = styled.ul`
-  height: calc(80vh - 24rem);
+  height: calc(100% - 250px);
   text-align: center;
   overflow: auto;
   margin: 15px;
